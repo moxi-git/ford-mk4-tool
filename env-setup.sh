c261ab22461b208ef89a5c1aae34f718e9c4784d
moxi <moxi@noreply.codeberg.org>
2025-07-15 02:58:32 +0200

    added env-setup.sh script, and requirements.txt

---
+++ env-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# env-setup.sh - Set up and activate a virtual environment
+
+python -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
